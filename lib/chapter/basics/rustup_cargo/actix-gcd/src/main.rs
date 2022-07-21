814a3b6144659c284997f4f6143f556c4d1a4ed9
John Chen <john.hk.chen@gmail.com>
2022-07-21 08:19:34 -0700

    Add simple warp example from web

--- lib/chapter/basics/rustup_cargo/actix-gcd/src/main.rs
+++ lib/chapter/basics/rustup_cargo/actix-gcd/src/main.rs
@@ -1,63 +1,20 @@
-use actix_web::{middleware, web, App, HttpRequest, HttpServer};
+use actix_web::{get, App, HttpServer, http::header::ContentType, HttpResponse, Responder};
 
-async fn index(req: HttpRequest) -> &'static str {
-    println!("REQ: {req:?}");
-    "Hello world!"
+#[get("/")]
+async fn greet() -> impl Responder {
+    let body = "data"
+    HttpResponse::Ok()
+        .content_type(ContentType::html())
+        .body(body)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    std::env::set_var("RUST_LOG", "actix_web=info");
-    env_logger::init();
-
     HttpServer::new(|| {
         App::new()
-            // enable logger
-            .wrap(middleware::Logger::default())
-            .service(web::resource("/index.html").to(|| async { "Hello world!" }))
-            .service(web::resource("/").to(index))
+            .service(greet)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("127.0.0.1", 4000))?
     .run()
     .await
-}
-
-// fn get_index() -> HttpResponse {
-//     HttpResponse::Ok()
-//         .content_type("text/html")
-//         .body(
-//             r#"
-//                 <title>GCD Calculator</title>
-//                 <form action="/gcd" method="post">
-//                     <input type="text" name="n" />
-//                     <input type="text" name="m" />
-//                     <button type="submit">Compute GCD</button>
-//                 </form>
-//             "#,
-//         )
-// }
-
-#[cfg(test)]
-mod tests {
-    use actix_web::body::to_bytes;
-    use actix_web::dev::Service;
-    use actix_web::{http, test, web, App, Error};
-
-    use super::*;
-
-    #[actix_web::test]
-    async fn test_index() -> Result<(), Error> {
-        let app = App::new().route("/", web::get().to(index));
-        let app = test::init_service(app).await;
-
-        let req = test::TestRequest::get().uri("/").to_request();
-        let resp = app.call(req).await?;
-
-        assert_eq!(resp.status(), http::StatusCode::OK);
-
-        let response_body = resp.into_body();
-        assert_eq!(to_bytes(response_body).await?, r##"Hello world!"##);
-
-        Ok(())
-    }
 }
